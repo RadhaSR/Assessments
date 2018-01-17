279381653a2480538d2cccfa250ad733ebfc8734
Radha SR <radhasr.ravi@gmail.com>
2018-01-17 22:42:02 +0530

    Arrays updated

--- sum_of_arrays.rb
+++ sum_of_arrays.rb
@@ -1,8 +1,9 @@
 num = [10, 25, 30, 45]
 
-puts sum = num.inject(:+)
+############# Sum of array elements
+puts "Sum of array elements #{ sum = num.inject(:+)}"
 
-
+############# Sum of even numbers in the arrays
 n1 = 0
 num.each do |n|
 	if n % 2 == 0
@@ -10,20 +11,23 @@ num.each do |n|
 		n1 = n1 + n
 	end
 end
-puts n1
+puts "Sum of even numbers in the array #{n1}"
 
-# num1 = []
-# num.each_with_index do |n, index|
-# 	if (index % 2 != 0)
-# 		num1.push(n)
-# 	end
-# end
-# puts num1.inject(:+)
+############# Sum of values in the odd index
+num1 = []
+num.each_with_index do |n, index|
+	if (index % 2 != 0)
+		num1.push(n)
+	end
+end
+puts num1.inject(:+)
 
-# n2 = []
-# num.each do |n|
-# 	if n <= 30
-# 		n2.push(n)
-# 	end
-# end
-# puts n2.inject(:+)
+
+############# Sum of numbers less than 0r equal to 30
+n2 = []
+num.each do |n|
+	if n <= 30
+		n2.push(n)
+	end
+end
+puts n2.inject(:+)
